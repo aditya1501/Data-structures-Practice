07a9870cb2f327c1445c6b24d35d408e5872221d
Aditya Rao <adityarao1501@gmail.com>
2021-02-16 10:13:28 +0530

    added-11-basic-problems

---
+++ basic codes/basic.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/*1print the number entered by user
+	int n;
+	cin>>n;
+	cout<<n;
+	*/
+	//2 add two numbers
+	// int num1,num2;
+	// cin>>num1>>num2;
+	// cout<<"sum::"<<num1+num2;
+
+	//program 3:compute quotient and remainder
+	// int divisor,dividend,quotient,remainder;
+	// cin>>dividend>>divisor;
+	// quotient=dividend/divisor;
+	// remainder=dividend%divisor;
+	// cout<<"quotient="<<quotient<<endl;
+	// cout<<"remainder="<<remainder<<endl;
+
+	//4. program to find the size of a variable
+	// cout<<"Sizeof char--->"<<sizeof(char)<<"\n";
+	// cout<<"Sizeof int--->"<<sizeof(int)<<"\n";
+	// cout<<"Sizeof float--->"<<sizeof(float)<<"\n";
+	// cout<<"Sizeof double--->"<<sizeof(double)<<"\n";
+
+	//5. program to swap two variables
+	// int a=5;
+	// int b=6;
+	// int temp;
+	// temp=a;
+	// a=b;
+	// b=temp;
+	// cout<<a<<" "<<b;
+
+	// 6. to find out the ascii value of character
+	// char c;
+	// cin>>c;
+	// cout<<"ASCII value of c="<<int(c);
+
+	//7.multiplication of two numbers
+	// int a,b;
+	// cin>>a>>b;
+	// cout<<a*b<<endl;
+
+	//8.check whether even or odd
+	// int a;
+	// cin>>a;
+	// if(a%2==0)cout<<"even";
+	// else
+	// 	cout<<"odd";
+
+	// 9.program to check whether vowel or constant
+	// char c;
+	// cin>>c;
+	// if(c=='a'|| c=='e' || c=='i' || c=='o' || c=='u')
+	// 	cout<<"small vowel"<<endl;
+	// else if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+	// 	cout<<"large vowel"<<endl;
+	// else
+	// 	cout<<"consonants"<<endl;
+	
+	//10 largest amongest 3
+	// int a,b,c;
+	// cin>>a>>b>>c;
+	// if(a>=b and a>=c)
+	// 	//a 
+	// 	cout<<a<<endl;
+	// if(b>=a and b>=c)
+	// 	cout<<b<<endl;
+	// if(c>=a and c>=b)
+	// 	cout<<c<<endl;
+
+	//11 find all the roots of quadratic equation
+	// int a;//coeficient of x2
+	// int b;//coeficient of x
+	// int c;//constant term of quadratic equation
+	// cin>>a>>b>>c;
+	// int d=(b*b)-(4*a*c);
+	// if(d>=0)
+	// {
+	// 	d=sqrt(d);
+	// 	cout<<"Real roots are possibel"<<endl;
+	// 	cout<<"root1-->"<<((-1*b)+d)/2*a<<endl;
+	// 	cout<<"root2-->"<<((-1*b)-d)/2*a<<endl;
+	// }
+	// else{
+	// 	cout<<"real roots are not possible"<<endl;
+	// }
+
+	//
+	return 0;
+}
